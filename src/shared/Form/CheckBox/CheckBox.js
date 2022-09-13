3807dc0336cbac3dcdd97e4f7c547994121c53db
padala_sita <padalasita7@gmail.com>
2022-09-13 11:39:41 +0530

    refactor

---
+++ src/shared/Form/CheckBox/CheckBox.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+function Checkbox(props) {
+  const [checked, setChecked] = useState(props.checked);
+  const handleOnChange = (event) => {
+    if (props.onChange) {
+      props.onChange(event);
+    }
+    setChecked((checked) => !checked);
+  };
+
+  return (
+    <div className="cd-checkbox-container">
+      <input
+        type="checkbox"
+        id="Checkbox"
+        className="cd-check-box"
+        checked={checked}
+        onChange={handleOnChange}
+      />
+
+      <label htmlFor="Checkbox" className="cd-label">
+        {" "}
+        {props.label}
+      </label>
+    </div>
+  );
+}
+export default Checkbox;
